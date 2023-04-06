ab8c74b5e8406ce38da95373a4cff122fd53c357
Denys Horman <denys.horman@pm.me>
2023-04-06 23:35:10 +0300

    Add handlers to detect app installation and standalone mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { GoogleAnalyticsService } from 'ngx-google-analytics';
 
 @Component({
   selector: 'app-root',
   template: '<app-eth-addr-generator></app-eth-addr-generator>',
   styleUrls: [],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private gaService: GoogleAnalyticsService) {}
+
+  ngOnInit(): void {
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+      this.gaService.event('display_mode_standalone');
+    }
+  }
+
+  @HostListener('window:appinstalled')
+  onAppInstalled() {
+    this.gaService.event('app_installed');
+  }
+}
