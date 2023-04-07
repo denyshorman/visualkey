f8f3e98f3d9e5e7c0323df026853786dc8b062eb
Denys Horman <denys.horman@pm.me>
2023-04-07 13:01:52 +0300

    Change the location of the eth-config.json file

--- src/app/config/eth-chain-config.service.ts
+++ src/app/config/eth-chain-config.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import ethConfig from '../../assets/eth-config.json';
+import ethConfig from './eth-config.json';
 
 @Injectable({
   providedIn: 'root',
