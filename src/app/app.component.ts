4f141d1f3e9960b83b4f633b5264d615cf1567bd
Denys Horman <denys.horman@pm.me>
2025-07-13 18:24:06 +0300

    Add dynamic meta tag update and removal based on route changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { afterNextRender, Component, DestroyRef } from '@angular/core';
 import { AnalyticsService } from './services/analytics.service';
 import { ThemeService } from './services/theme.service';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { Toast } from 'primeng/toast';
 import { ConfirmDialog } from 'primeng/confirmdialog';
+import { Meta } from '@angular/platform-browser';
+import { filter, map, mergeMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -28,6 +30,8 @@ export class AppComponent {
     private themeService: ThemeService,
     private analyticsService: AnalyticsService,
     private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private metaTagService: Meta,
     private destroyRef: DestroyRef,
   ) {
     afterNextRender(() => {
@@ -37,13 +41,34 @@ export class AppComponent {
         this.analyticsService.trackEvent('app_display_mode_standalone');
       }
 
-      const routerEventsSubscription = this.router.events.subscribe(event => {
-        if (event instanceof NavigationEnd) {
+      const routerEventsSubscription = this.router.events
+        .pipe(
+          filter(event => event instanceof NavigationEnd),
+          map(() => {
+            let route = this.activatedRoute;
+
+            while (route.firstChild) {
+              route = route.firstChild;
+            }
+
+            return route;
+          }),
+          filter(route => route.outlet === 'primary'),
+          mergeMap(route => route.data),
+        )
+        .subscribe(data => {
+          const description = data['description'];
+
+          if (description) {
+            this.metaTagService.updateTag({ name: 'description', content: description });
+          } else {
+            this.metaTagService.removeTag("name='description'");
+          }
+
           this.analyticsService.trackEvent('page_view', {
             page_location: window.location.href,
           });
-        }
-      });
+        });
 
       this.destroyRef.onDestroy(() => {
         routerEventsSubscription.unsubscribe();
